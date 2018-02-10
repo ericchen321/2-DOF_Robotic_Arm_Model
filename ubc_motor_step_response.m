f1f76919c0ec0d7ece21f7b4cb09f8a7e924a490
Eric Chen <cgx1997@outlook.com>
2018-02-10 15:32:38 -0800

    test 11 analyzed

--- ubc_motor_step_response.m
+++ ubc_motor_step_response.m
@@ -1,40 +1,48 @@
 clear all;
 
-% with noise
-fileID0 = fopen('ubc_motor_step_response.txt','r');
-formatSpec = '%f %f %f';
-sizeA = [3 inf];
+% read values
+fileID0 = fopen('ubc_motor_step_response_v11_@10V.txt','r');
+formatSpec = '%f,%f';
+sizeA = [2 inf];
 A = fscanf(fileID0,formatSpec,sizeA);
 A = A';
 fclose(fileID0);
-timeVec = 0;
-angleVec = 0;
-voltVec = 0;
 
-for i=2:(size(A,1)-1)
-    if(A(i,1)>A(i-1,1))
-        timeVec = [timeVec; A(i,1)];
-        angleVec = [angleVec; A(i,2)];
-        voltVec = [voltVec; A(i,3)];
-    end
-end
-
-timeVec=timeVec(1:300);
-angleVec=angleVec(1:300)*(pi/180);
-voltVec=voltVec(1:300);
+% get time and angle and speed and fitted xf
+timeVec = (A(:, 2) - A(2,1)*ones(size(A,1),1))*10^(-6);
+angleVec = A(:, 1) * (3.6/4.0) * pi/180;
+%plot(timeVec,angleVec);
 speedVec = diff(angleVec)./diff(timeVec);
+speedVec = movmean(speedVec,3);
 
-timeVec = timeVec(1:299);
-speedVec = speedVec - mean(speedVec(101:299));
+i = 1;
+while (angleVec(i) == 0)
+    i = i+1;
+end    
+voltVec = [zeros(i-1,1) ; 10*ones((size(A,1)-1)-(i-1),1)];
 
-R=2.768;
-L=0.0019;
-J=2.92*10^(-5);
-Kt=0.01401;
-Ke=0.0152;
-B=3.58*10^(-7);
-s = tf('s');
-secOrderXF = (Kt/(L*J))/(s^2 + (B/J+R/L)*s + (R*B+Kt*Ke)/(L*J));
-plot(timeVec, (121.8-121.8*exp(-2.66*timeVec))*0.535);
+timeVec = timeVec(1: (size(A,1)-1));
+Ts = timeVec(end)/length(timeVec);
+estXFData = iddata(speedVec,voltVec,Ts);
+estXF = tfest(estXFData,2,0);
+
+timeVecShifted = timeVec(i-1:end);
+timeVecShifted = timeVecShifted - timeVecShifted(1,1);
+voltVecShifted = voltVec(i-1:end);
+speedVecShifted = speedVec(i-1:end);
+
+% get xf using measured B, J and stuff
+%R=2.768;
+%L=0.0012;
+%J=2.92*10^(-5);
+%Kt=0.0141;
+%Ke=0.0141;
+%B=3.58*10^(-7);
+%s = tf('s');
+%modelXF = (Kt/(L*J))/(s^2 + (B/J+R/L)*s + (R*B+Kt*Ke)/(L*J));
+
+step(10 * estXF);
 hold on;
-step(secOrderXF);
+plot(timeVecShifted, speedVecShifted);
+hold off;
+%step(modelXF);
